36b3eee83332300b76394aabc78e1ae2489498a4
Antoine RICHARD <arichard@sqli.com>
2013-04-04 23:00:31 +0200

    initial state for step 1

--- app/js/app.js
+++ app/js/app.js
@@ -2,5 +2,12 @@
 angular
 	.module('moviesApp', [])
 	.config(function($routeProvider) {
-		$routeProvider.when('/?', { controller: '?', templateUrl: '?' });
+		$routeProvider.when('/movies', { controller: 'MoviesCtrl', templateUrl: 'partials/movies.html' });
+	})
+	.controller('MoviesCtrl', function($scope) {
+		$scope.movies = [
+			{ title: "Reservoir Dogs", year: 1992 },
+			{ title: "Pulp Fiction", year: 1994 },
+			{ title: "Jackie Brown", year: 1997 }
+		];
 	});
