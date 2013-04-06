89bc7856af67d7b974e547660dcf5614a26d60b5
Antoine RICHARD <arichard@sqli.com>
2013-04-06 14:00:11 +0200

    moved movies.json

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,7 @@
  * Movies list.
  */
 app.controller('MoviesCtrl', ['$http', '$scope', function($http, $scope) {
-	$http.get('/data/movies.json/$').success(function(movies) {
+	$http.get('/server/data/movies.json/$').success(function(movies) {
 		$scope.movies = movies;
 	});
 }]);
