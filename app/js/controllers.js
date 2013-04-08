5136533b7c7216aa5ce76a39d9eeab32ebfb72e6
Antoine RICHARD <arichard@sqli.com>
2013-04-08 22:22:47 +0200

    added controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,28 @@
  * Movies list.
  */
 app.controller('MoviesCtrl', ['$http', '$scope', function($http, $scope) {
-	$http.get('/server/data/movies.json/$').success(function(movies) {
+	$http.get('/server/data/movies.json/$')
+	.success(function(movies) {
 		$scope.movies = movies;
+	});
+}]);
+
+/**
+ * Movie info sheet.
+ */
+app.controller('MovieDetailCtrl', ['$http', '$scope', '$routeParams', 'starService', function($http, $scope, $routeParams, starService) {
+	$http.get('/data/movies.json/'+$routeParams.movieId+'/$') // Lightweight movie object
+	.success(function(lightMovie) {
+		$scope.movie = lightMovie;
+	});
+}]);
+
+/**
+ * Movie's actors.
+ */
+app.controller('MovieActorsCtrl', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams) {
+	$http.get('/data/movies.json/'+$routeParams.movieId) // Heavyweight movie object
+	.success(function(fullMovie) {
+		$scope.movie = fullMovie;
 	});
 }]);
