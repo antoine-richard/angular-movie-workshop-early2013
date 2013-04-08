b1be5553676580ebe4abf58b47b5bc4afeb15114
Antoine RICHARD <arichard@sqli.com>
2013-04-08 22:38:01 +0200

    updated controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,7 +13,7 @@ app.controller('MoviesCtrl', ['$http', '$scope', function($http, $scope) {
  * Movie info sheet.
  */
 app.controller('MovieDetailCtrl', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams) {
-	$http.get('/data/movies.json/'+$routeParams.movieId+'/$') // Lightweight movie object
+	$http.get('/server/data/movies.json/'+$routeParams.movieId+'/$') // Lightweight movie object
 	.success(function(lightweightMovie) {
 		$scope.movie = lightweightMovie;
 	});
@@ -23,7 +23,7 @@ app.controller('MovieDetailCtrl', ['$http', '$scope', '$routeParams', function($
  * Movie's actors.
  */
 app.controller('MovieActorsCtrl', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams) {
-	$http.get('/data/movies.json/'+$routeParams.movieId) // Heavyweight movie object
+	$http.get('/server/data/movies.json/'+$routeParams.movieId) // Heavyweight movie object
 	.success(function(fullMovie) {
 		$scope.movie = fullMovie;
 	});
