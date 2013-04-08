007dd87b0ecc521fd71b9bccae9951b933d897c6
Antoine RICHARD <arichard@sqli.com>
2013-04-08 21:58:38 +0200

    added routes

--- app/js/app.js
+++ app/js/app.js
@@ -9,5 +9,9 @@ var app = angular.module('moviesApp', []);
  * Routes
  */
 app.config(function($routeProvider) {
- 	$routeProvider.when('/movies', { controller: 'MoviesCtrl', templateUrl: 'partials/movies.html' });
+ 	$routeProvider
+ 		.when('/movies', 					{ controller: 'MoviesCtrl',			templateUrl: 'partials/movies.html' })
+ 		.when('/movies/:movieId', 			{ controller: 'MovieDetailCtrl', 	templateUrl: 'partials/movie.html'  })
+ 		.when('/movies/:movieId/actors', 	{ controller: 'MovieActorsCtrl',	templateUrl: 'partials/actors.html' })
+ 		.otherwise(							{ redirectTo: '/movies' });
 });
